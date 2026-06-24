01677abb0a3a82151e0c275ceaa26761835d7e28
detdamrong8 <467129@wsk.ac.th>
2026-06-24 08:56:53 +0000

    program02.py

---
+++ program02.py
@@ -0,0 +1,8 @@
+print("พายมึงต้อ")
+width=float(input("กรอกความกว้าง: "))
+length=float(input("กรอกความยาว: "))
+area=width * length
+perimeter=2 * (width + length)
+print("พื้นที่ =", area)
+print("เส้นรอบรูป =", perimeter)
+print("Auhor by detdamrong4 610")
